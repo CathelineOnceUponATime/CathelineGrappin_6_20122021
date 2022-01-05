f2a01b3f236882d21a1c17071b3911305f3d95b4
CathelineOnceUponATime <Catheline.27.69@gmail.com>
2022-01-05 15:30:45 +0100

    Finalisation responsive et formulaire

--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -1,5 +1,15 @@
 /* global fetch, Photographe */
 /* eslint no-undef: "error" */
+class Erreur {
+  constructor (id, message, presenceErreur, idErreur, typeErreur) {
+    this.id = id
+    this.message = message
+    this.presenceErreur = presenceErreur
+    this.idErreur = idErreur
+    this.typeErreur = typeErreur
+  }
+}
+
 let fichPhotographes
 let photographeC
 const prenom = document.getElementById('prenom')
@@ -9,6 +19,19 @@ const message = document.getElementById('message')
 const modal = document.getElementsByClassName('modal')
 const logo = document.getElementsByClassName('logo')
 const main = document.getElementById('main')
+const tErreurs = []
+
+// Nouvel objet pour chaque erreur
+const ePrenom = new Erreur('prenom', 'Merci de saisir minimum deux caractères pour le prénom', false, 'prenomErreur', 'saisie')
+const eNom = new Erreur('nom', 'Merci de saisir minimum deux caractères pour le Nom', false, 'nomErreur', 'saisie')
+const eMail = new Erreur('email', 'Merci de saisir une adresse e-mail valide', false, 'emailErreur', 'email')
+const eMessage = new Erreur('message', 'Merci de saisir votre message', false, 'messageErreur', 'saisie')
+
+// remplissage du tableau avec chaque erreur
+tErreurs.push(ePrenom)
+tErreurs.push(eNom)
+tErreurs.push(eMail)
+tErreurs.push(eMessage)
 
 function displayModal () { // eslint-disable-line no-unused-vars
   const lienSite = window.location.href
@@ -21,11 +44,6 @@ function displayModal () { // eslint-disable-line no-unused-vars
   }
   const bFermer = document.getElementsByClassName('contact_button')
   bFermer[1].style.marginTop = '25px'
-  /* main.style.filter = 'blur(5px)'
-  main.style.pointerEvents = 'none'
-  main.tabIndex = -1
-  logo[0].style.filter = 'blur(5px)'
-  logo[0].style.pointerEvents = 'none' */
   main.style.display = 'none'
   logo[0].style.display = 'none'
   modal[0].style.display = 'flex'
@@ -79,6 +97,90 @@ function closeModal () { // eslint-disable-line no-unused-vars
   logo[0].style.pointerEvents = 'auto' */
 }
 
+function montreErreur (eErreur) {
+  const elt = document.getElementById(eErreur.id)
+  const eltParent = elt.parentElement
+  const erreur = document.createElement('label')
+  erreur.classList.add('erreur')
+  erreur.innerHTML = eErreur.message
+  erreur.id = eErreur.idErreur
+  eltParent.insertBefore(erreur, elt.nextElementSibling)
+
+  if (eErreur.typeErreur !== 'checkbox') {
+    elt.style.border = '3px solid red'
+  }
+}
+
+// Fonction permettant de supprimer du DOM
+// le label créé auparavant
+function fermeErreur (eErreur) {
+  const elt = document.getElementById(eErreur.id)
+  const eltParent = elt.parentElement
+  const erreur = document.getElementById(eErreur.idErreur)
+  eltParent.removeChild(erreur)
+
+  if (eErreur.typeErreur !== 'checkbox') {
+    elt.style.border = '0.8px solid #ccc'
+  }
+}
+
+function verificationFormulaire (idChamp, contenuChamp) {
+  let bErreur = false
+  for (let i = 0; i < tErreurs.length; i++) {
+    if (tErreurs[i].id === idChamp) {
+      if (tErreurs[i].typeErreur === 'email') {
+        let bValide = false
+        // boucle permettant de parcourir chaque caractère de l'email
+        for (let j = 0; j < (contenuChamp.length); j++) {
+          // Si on trouve un @
+          if (contenuChamp.charAt(j) === '@') {
+            if (j < (contenuChamp.length - 4)) {
+              for (let k = j; k < (contenuChamp.length - 2); k++) {
+                // Si il y a un point après @
+                if (contenuChamp.charAt(k) === '.') {
+                  bValide = true
+                  break
+                }
+              }
+            }
+          }
+        }
+        bErreur = !bValide
+      } else {
+        if (contenuChamp === '' || contenuChamp.length < 2) {
+          bErreur = true
+        }
+      }
+      if (bErreur) {
+        // S'il n'y a pas déjà une erreur
+        // On crée une erreur et on actualise l'objet
+        if (!tErreurs[i].presenceErreur) {
+          tErreurs[i].presenceErreur = true
+          montreErreur(tErreurs[i])
+          break
+        }
+      } else {
+        if (tErreurs[i].presenceErreur) {
+          tErreurs[i].presenceErreur = false
+          fermeErreur(tErreurs[i])
+          break
+        }
+      }
+    }
+  }
+}
+
+function presenceErreurChamp () {
+  let bErreur = false
+  for (let i = 0; i < tErreurs.length; i++) {
+    if (tErreurs[i].presenceErreur && !bErreur) {
+      bErreur = true
+      break
+    }
+  }
+  return bErreur
+}
+
 function videFormulaire () {
   prenom.value = ''
   nom.value = ''
@@ -86,14 +188,26 @@ function videFormulaire () {
   message.value = ''
   const titre = document.getElementById('titre')
   titre.innerHTML = 'Contactez-moi'
+  for (let i = 0; i < tErreurs.length; i++) {
+    if (tErreurs[i].presenceErreur) {
+      tErreurs[i].presenceErreur = false
+      fermeErreur(tErreurs[i])
+    }
+  }
 }
 
 function envoiMessage () { // eslint-disable-line no-unused-vars
   const form = document.getElementsByTagName('form')
   form[0].action = 'photographer.html?id=' + photographeC.id
-
-  console.log(' prénom : ' + prenom.value + '\n nom : ' + nom.value + '\n email : ' + email.value + '\n message : ' + message.value)
-
-  closeModal()
-  return false
+  verificationFormulaire(prenom.id, prenom.value)
+  verificationFormulaire(nom.id, nom.value)
+  verificationFormulaire(email.id, email.value)
+  verificationFormulaire(message.id, message.value)
+  if (presenceErreurChamp()) {
+    return false
+  } else {
+    console.log(' prénom : ' + prenom.value + '\n nom : ' + nom.value + '\n email : ' + email.value + '\n message : ' + message.value)
+    closeModal()
+    return false
+  }
 }
