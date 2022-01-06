699a932afce21021804849251d095a873769c69e
CathelineOnceUponATime <Catheline.27.69@gmail.com>
2022-01-06 15:36:34 +0100

    Rajout article page d'accueil, tabulation correcte

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -6,8 +6,9 @@ function photographerFactory (data) { // eslint-disable-line no-unused-vars
     const lien = document.createElement('a')
     const img = document.createElement('img')
     const h2 = document.createElement('h2')
+    const articleInfo = document.createElement('article')
     const tagLine = document.createElement('p')
-    const villePays = document.createElement('p')
+    const villePays = document.createElement('h3')
     const prix = document.createElement('p')
     img.setAttribute('src', picture)
     img.setAttribute('alt', data.description)
@@ -15,20 +16,20 @@ function photographerFactory (data) { // eslint-disable-line no-unused-vars
     lien.href = 'photographer.html?id=' + data.id
     lien.tabIndex = (nbCart + 3)
     tagLine.textContent = data.tagLine
-    tagLine.tabIndex = (nbCart + 4)
     tagLine.classList.add('tagLine')
     villePays.textContent = data.ville + ', ' + data.pays
-    villePays.tabIndex = (nbCart + 4)
     villePays.classList.add('ville')
     prix.textContent = data.prix + '€ / jour'
     prix.classList.add('prix')
-    prix.tabIndex = (nbCart + 4)
     article.appendChild(lien)
     lien.appendChild(img)
     lien.appendChild(h2)
-    article.appendChild(villePays)
-    article.appendChild(tagLine)
-    article.appendChild(prix)
+    articleInfo.tabIndex = (nbCart + 4)
+    articleInfo.classList.add('info')
+    articleInfo.appendChild(villePays)
+    articleInfo.appendChild(tagLine)
+    articleInfo.appendChild(prix)
+    article.appendChild(articleInfo)
     return (article)
   }
   return { picture, data, getUserCardDOM }
